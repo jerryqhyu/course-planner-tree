b98bd310321163401dc7ae2aba9452383c9b5c63
sinoforever <sinoforever@outlook.com>
2014-11-09 20:39:08 -0500

    alpha1

---
+++ course.py
@@ -0,0 +1,137 @@
+# Assignment 2 - Course Planning!
+#
+# CSC148 Fall 2014, University of Toronto
+# Instructor: David Liu
+# ---------------------------------------------
+# STUDENT INFORMATION
+#
+# List your group members below, one per line, in format
+# <full name>, <utorid>
+#
+#
+#
+# ---------------------------------------------
+
+
+class UntakeableError(Exception):
+    pass
+
+
+class PrerequisiteError(Exception):
+    pass
+
+"""Course prerequisite data structure.
+
+This module contains the class that should store all of the
+data about course prerequisites and track taken courses.
+Note that by tracking "taken" courses, we are restricting the use
+of this class to be one instance per student (otherwise,
+"taken" doesn't make sense).
+
+Course: a course and its prerequisites.
+"""
+
+
+class Course:
+    """A tree representing a course and its prerequisites.
+
+    This class not only tracks the underlying prerequisite relationships,
+    but also can change over time by allowing courses to be "taken".
+
+    Attributes:
+    - name (str): the name of the course
+    - prereqs (list of Course): a list of the course's prerequisites
+    - taken (bool): represents whether the course has been taken or not
+    """
+
+    # Core Methods - implement all of these
+    def __init__(self, name, prereqs=None):
+        """ (Course, str, list of Courses) -> NoneType
+
+        Create a new course with given name and prerequisites.
+        By default, the course has no prerequisites (represent this
+        with an empty list, NOT None).
+        The newly created course is not taken.
+        """
+        self.name = name
+        if prereqs is None:
+            self.prereqs = []
+        else:
+            self.prereqs = prereqs
+        self.taken = False
+
+    def is_takeable(self):
+        """ (Course) -> bool
+
+        Return True if the user can take this course.
+        A course is takeable if and only if all of its prerequisites are taken.
+        """
+        if self.prereqs == []:
+            return True
+        elif self.taken == True:
+            return True
+        else:
+            for course in self.prereqs:
+                if course.taken is False:
+                    return False
+            return True
+
+    def take(self):
+        """ (Course) -> NoneType
+
+        If this course is takeable, change self.taken to True.
+        Do nothing if self.taken is already True.
+        Raise UntakeableError if this course is not takeable.
+        """
+        if self.taken is False:
+            if self.is_takeable() is True:
+                self.taken = True
+            else:
+                raise UntakeableError
+
+    def add_prereq(self, prereq):
+        """ (Course, Course) -> NoneType
+
+        Add a prereq as a new prerequisite for this course.
+
+        Raise PrerequisiteError if either:
+        - prereq has this course in its prerequisite tree, or
+        - this course already has prereq in its prerequisite tree
+        """
+        if prereq == self:
+            raise PrerequisiteError
+        elif prereq in self.prereqs:
+            raise PrerequisiteError
+        elif self in prereq.prereqs:
+            raise PrerequisiteError
+        else:
+            self.prereqs.append(prereq)
+
+    def missing_prereqs(self):
+        """ (Course) -> list of str
+
+        Return a list of all of the names of the prerequisites of this course
+        that are not taken.
+
+        The returned list should be in alphabetical order, and should be empty
+        if this course is not missing any prerequisites.
+        """
+        result = []
+        if self.prereqs == []:
+            return []
+        for prereq in self.prereqs:
+            if prereq.taken is False:
+                result += [prereq.name] + prereq.missing_prereqs()
+                result.sort()
+        return result
+
+    def __contains__(self, code):
+        if self.name == code:
+            return True
+        elif self.prereqs == []:
+            return False
+        else:
+            for cour in self.prereqs:
+                if code in cour:
+                    return True
+            return False
---
+++ planner.py
@@ -0,0 +1,160 @@
+# Assignment 2 - Course Planning!
+#
+# CSC148 Fall 2014, University of Toronto
+# Instructor: David Liu
+# ---------------------------------------------
+# STUDENT INFORMATION
+#
+# List your group members below, one per line, in format
+# <full name>, <utorid>
+#
+#
+#
+# ---------------------------------------------
+"""Program for helping users plan schedules.
+
+This module contains the main class that is used to interact with users
+who are trying to plan their schedules. It requires the course module
+to store prerequisite information.
+
+TermPlanner: answers queries about schedules based on prerequisite tree.
+"""
+from course import Course
+
+
+def parse_course_data(filename):
+    """ (str) -> Course
+
+    Read in prerequisite data from the file called filename,
+    create the Course data structures for the data,
+    and then return the root (top-most) course.
+
+    See assignment handout for details.
+    """
+    courses = {}
+    with open(filename,'r') as source:
+        for line in source:
+            classes = line.split()
+            if classes[0] not in courses:
+                courses[classes[0]] = Course(classes[0])
+            if classes[1] not in courses:
+                courses[classes[1]] = Course(classes[1])
+            if courses[classes[0]] not in courses[classes[1]].prereqs:
+                courses[classes[1]].add_prereq(courses[classes[0]])
+    top = None
+    for cour in courses:
+        boo = True
+        for compare in courses:
+            if courses[cour] in courses[compare].prereqs:
+                boo = False
+        if boo:
+            return courses[cour]
+
+class TermPlanner:
+    """Tool for planning course enrolment over multiple terms.
+
+    Attributes:
+    - course (Course): tree containing all available courses
+    """
+
+    def __init__(self, filename):
+        """ (TermPlanner, str) -> NoneType
+
+        Create a new term planning tool based on the data in the file
+        named filename.
+
+        You may not change this method in any way!
+        """
+        self.course = parse_course_data(filename)
+
+    def is_valid(self, schedule):
+        """ (TermPlanner, list of (list of str)) -> bool
+
+        Return True if schedule is a valid schedule.
+        Note that you are *NOT* required to specify why a schedule is invalid,
+        though this is an interesting exercise!
+        """
+        copy_course = copy(self.course)
+        actually_taken = []
+        if schedule == []:
+            return True
+        else:
+            for sch in schedule:
+                for cour in sch:
+                    if cour not in copy_course:
+                        return False
+                    else:
+                        if prereqs_taken(cour, copy_course):
+                            take(cour, copy_course)
+                            actually_taken.append(cour)
+                        else:
+                            return False
+            for i in range(len(actually_taken)):
+                for j in range(i+1,len(actually_taken)):
+                    if actually_taken[i] == actually_taken[j]:
+                        return False
+            return True
+
+
+    def generate_schedule(self, selected_courses):
+        """ (TermPlanner, list of str) -> list of (list of str)
+
+        Return a schedule containing the courses in selected_courses that
+        satisfies the restrictions given in the assignment specification.
+
+        You may assume that all the courses in selected_courses appear in
+        self.course.
+
+        If no valid schedule can be formed from these courses, return an
+        empty list.
+        """
+        copy_course = copy(self.course)
+        copy_want = selected_courses[:]
+        schedule = []
+        for i in range(len(selected_courses)):
+            sch = []
+            for cour in copy_want:
+                if prereqs_taken(cour, copy_course) and len(sch) < 5:
+                    sch.append(cour)
+            for course in sch:
+                take(course, copy_course)
+                copy_want.remove(course)
+            sch.sort()
+            if sch != []:
+                schedule.append(sch)
+        if len(copy_want) != 0:
+            schedule.append(copy_want)
+        if self.is_valid(schedule):
+            return schedule
+        else:
+            return []
+
+
+
+
+
+def copy(course):
+    return Course(course.name, course.prereqs)
+
+def take(code, course):
+    if course.name == code:
+        course.take()
+    elif course.prereqs == []:
+        pass
+    else:
+        for cour in course.prereqs:
+            take(code,cour)
+
+def prereqs_taken(code,course):
+    if course.name == code:
+        for cour in course.prereqs:
+            if cour.taken == False:
+                return False
+        return True
+    elif course.prereqs == []:
+        return True
+    else:
+        for cour in course.prereqs:
+            if not prereqs_taken(code,cour):
+                return False
+        return True
