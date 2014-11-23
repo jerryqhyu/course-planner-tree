b1257e16d6078fd6a1205c47668ece95fe6ce7db
LucasHu <ming.sebastian.unsw@gmail.com>
2014-11-22 19:55:17 -0500

    Update planner_Lucas.py

--- planner_Lucas.py
+++ planner_Lucas.py
@@ -52,7 +52,8 @@ def parse_course_data(filename):
                     course_list[course_name[1]].add_prereq(course_list[course_name[0]])
                     
                 else:
-                    if course_list[course_name[0]] not in course_list[course_name[1]].prereqs:
+                    if course_list[course_name[1]].__contains__(course_name[0]) == False:
+                        
                         course_list[course_name[1]].add_prereq(course_list[course_name[0]])
     return top_course(course_list)
 
@@ -60,9 +61,9 @@ def top_course(course_list):
     for class1 in course_list:
         count = 0
         for class2 in course_list:
-            if course_list[class1] in course_list[class2].prereqs:
+            if course_list[class2].__contains__(course_list[class1].name):
                 count += 1
-        if count == 0:
+        if count == 1:
             return course_list[class1]
             
 class TermPlanner:
@@ -89,6 +90,8 @@ class TermPlanner:
         Note that you are *NOT* required to specify why a schedule is invalid,
         though this is an interesting exercise!
         """
+        #reset_tree(self.course)
+        
         exist_count = 0 
         master_course_list = []
         for course_item_list in schedule:
@@ -102,24 +105,48 @@ class TermPlanner:
             for j in range(i+1, len(master_course_list)):
                 if master_course_list[i] == master_course_list[j]:
                     no_duplicate_count += 1
-            
+        pre_list = []
+        intree_count = 0
         if exist_count == 0 and no_duplicate_count == 0:            
             for level in range(len(schedule)):
-                if level == 0:
-                    for i in range(len(schedule[level])):
-                        item = schedule[level][i]
-                        if if_is_leaf(self.course, item) == False:
-                            return False
-                        else:    
-                            take_the_course(self.course, item)
-                else:
-                    for i in range(len(schedule[level])):
-                        item = schedule[level][i]
-                        if course_takeable(self.course, item) == False:
-                            return False
-                        else:    
-                            take_the_course(self.course, item)
-            return True     
+                #if level == 0:
+                    #for i in range(len(schedule[level])):
+                        #item = schedule[level][i]
+                        #if if_is_leaf(self.course, item) == False:
+                        #if self.course.find_prereq_course(item).prereqs == []:
+                            #return False
+                            #intree_count += 1
+                        #else:    
+                            #take_the_course(self.course, item)
+                    #pre_list += schedule[level] 
+                            
+                #else:
+                    #for i in range(len(schedule[level])):
+                        #item = schedule[level][i]
+                        
+                        #if course_takeable(self.course, item) == False:
+                            #return False
+                        #if is_takeable
+                            #intree_count += 1
+                        #else:    
+                            #take_the_course(self.course, item)
+                            #pre_list.append(lst)
+                for i in range(len(schedule[level])):
+                    item = schedule[level][i]
+                    item_course = find_prereq_course(self.course,item)
+                    if not item_course.is_takeable():
+                        if not biglst_smal_lst(pre_list,item_course.missing_prereqs()):
+                            intree_count += 1
+                            
+                        
+                    
+                pre_list += schedule[level]
+                
+                
+            if intree_count == 0:
+                return True
+            else:
+                return False
         else:
             return False
         
@@ -137,12 +164,26 @@ class TermPlanner:
         If no valid schedule can be formed from these courses, return an
         empty list.
         """
-        
+        reset_tree(self.course)
         if test_duplicate(selected_courses) == False:
             return []
         elif exist_in_tree(self.course, selected_courses) == False:
             return []
-        else:             
+        else: 
+            for course_code in selected_courses:
+                item_course = find_prereq_course(self.course, course_code)
+                #if item_course.taken == True:
+                    #item_course.taken = False
+                    
+                if biglst_smal_lst(selected_courses,item_course.missing_prereqs()) == False:
+                    return []
+               
+            
+            
+            
+            
+            
+            
             enrolled_list = []                    
             while selected_courses != []:
                 row_list = []
@@ -215,8 +256,46 @@ def exist_in_tree(top_course,lst):
         return True
     else:
         return False
+    
+def find_prereq_course(top_course, name):
+    if top_course.name == name:
+        return top_course
+    for items in top_course.prereqs:
+        find = find_prereq_course(items,name)
+        if find:
+            return find
+    return False
 
+def reset_tree(top_course):
+    if top_course.taken == True:
+        top_course.taken = False
+    for items in top_course.prereqs:
+        reset_tree(items)
+                
+def biglst_smal_lst(lst1,lst2):
+    count = 0
+    for items in lst2:
+        if items not in lst1:
+            count += 1
+    if count == 0:
+        return True
+    else:
+        return False
+    
+    
+    
+#def all_pre(top_course,course_code):
+    #if top_course.name == course_code:
+        #return top_course.missing_prereqs()
+    #else:
+        #for items in top_course.prereqs:
+            #if item
+            #return all_pre(items, course_code)
+            
         
+#def exist_in_tree(course1, course2):
+    #exist_count = 0
+    #if course2.name == course1.name
 
 
         
